2698671c1a8619e43dcd12dcba062eef2f6cba98
Oleg Volkov <olegv142@gmail.com>
2026-08-06 12:48:20 +0300

    Fix archive folder

--- standalone/build_sfx.bat
+++ standalone/build_sfx.bat
@@ -2,4 +2,5 @@ pyinstaller dmm-tools.spec --noconfirm
 
 for /f "delims=" %%i in ('python ../version.py') do set "Ver=%%i"
 
-7z a -sfx7z.sfx dist/dmm-tools-%Ver%.exe dist/dmm-tools
+cd dist
+7za a -sfx7z.sfx dmm-tools-%Ver%.exe dmm-tools
