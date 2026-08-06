07694983a01d6335a55237bb6b1b887317a56755
Oleg Volkov <olegv142@gmail.com>
2026-08-06 22:39:58 +0300

    Emphasize Start link

--- standalone/scripts/create_links.bat
+++ standalone/scripts/create_links.bat
@@ -6,7 +6,7 @@ pushd ..
 if not exist data mkdir data
 popd
 
-call .mk_link.bat ut61xp-start ut61xp-start.exe
+call .mk_link.bat ut61xp-Start ut61xp-start.exe
 call .mk_link.bat ut61xp-plot  ut61xp-plot.exe
 call .mk_link.bat ut61xp-hist  ut61xp-hist.exe
 call .mk_link.bat ut61xp-stat  ut61xp-stat.exe
